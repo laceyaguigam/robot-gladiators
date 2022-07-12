02393001d61994db03719a2c9dd823f60c9251f1
lacey aguigam <laceyaguigam@yahoo.com>
2022-07-12 12:02:23 -0700

    added basic js functions to js file

---
+++ assets/js/game.js
@@ -0,0 +1,24 @@
+
+//prompt, created a text box for user to respond (user input) and browser saves info
+
+var playerName = window.prompt("What is your robot's name?");
+
+//Note the lack of quotation marks around playerName
+console.log(playerName);    
+
+console.log("This logs a string, good for leaving yourself a message");
+
+//this will do math and log 20
+console.log(10+10);
+
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+//this creates a function named "fight" 
+
+function fight() {    //declared function
+    window.alert("The fight has begun!");
+    
+}
+
+//fight();    //called function 
